92e583eeeb686c269ee10856b5a591eff92b9fba
Shane Talty <61328623+shanetalty@users.noreply.github.com>
2025-01-23 10:34:24 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.querySelector('.scroll-icon').addEventListener('click', function() {
-    const currentSection = document.querySelector('.award-section:not(.hidden)');
-    
-    // Hide the current section
-    currentSection.classList.add('hidden');
-    
-    // Show the next section
-    let nextSection = currentSection.nextElementSibling;
-    if (nextSection) {
-        nextSection.classList.remove('hidden');
-    }
-});
+// Navigation function
+function goToPage(pageNumber) {
+    // Hide all pages
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => section.classList.add('hidden'));
+
+    // Show the page we're navigating to
+    document.getElementById('page' + pageNumber).classList.remove('hidden');
+}
+
+// Initial page load
+goToPage(1); // Start at page 1
